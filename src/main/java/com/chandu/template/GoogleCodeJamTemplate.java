45bbcdfe473e1b002045bc272c2c6e8f6e401e3d
Chandu <cchhaanndduu@gmail.com>
2013-04-17 14:59:56 -0700

    Modify Google Code Jam template

--- src/main/java/com/chandu/template/GoogleCodeJamTemplate.java
+++ src/main/java/com/chandu/template/GoogleCodeJamTemplate.java
@@ -25,21 +25,33 @@ public class GoogleCodeJamTemplate {
     private static final String caseNum = "Case #";
     private static final String colon = ": ";
     
+    private static long T = 0;
+    private static long lines = 0;
+    
     /*
      * This is the solution
      */
-    private static void solve() {
-        final int T = in.nextInt();
-        for (int t = 0; t < T; t += 1) {
+    private static int solve() {
+        T = in.nextInt();
+        for (long t = 0; t < T; t += 1) {
             // main logic here
-            printResult(t, Integer.toString(t));
+            printResult(t, Long.toString(t));
+        }
+        
+        if (lines != T) {
+            System.err.println("Something went wrong.");
+            System.err.println("Lines printed should be equal to the number of test case.");
+            return 1;
+        } else {
+            return 0;
         }
     }
     
     /*
      * Print the test case result
      */
-    private static void printResult(final int testCase, final String result) {
+    private static void printResult(final long testCase, final String result) {
+        lines += 1;
         out.println(caseNum + (testCase + 1) + colon + result);
     }
     
@@ -47,12 +59,14 @@ public class GoogleCodeJamTemplate {
      * Just change the fileName and run the program
      */
     public static void main(final String[] tcs) {
+        int result = 0;
+        
         try {
             
             in = new Scanner(new BufferedReader(new FileReader(fileName + inExt)));
             out = new PrintWriter(new BufferedWriter(new FileWriter(fileName + outExt)));
             
-            solve();
+            result = solve();
             
         } catch (FileNotFoundException ex) {
             System.err.println("[FileNotFoundException] >> " + ex.getMessage());
@@ -68,5 +82,7 @@ public class GoogleCodeJamTemplate {
                 out.close();
             }
         }
+
+        System.exit(result);
     }
 }
